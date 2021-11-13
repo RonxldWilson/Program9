c7a86a3a89838b369b60b745149566c3e169e204
RonxldWilson <ronaldwilson00000@gmail.com>
2021-11-14 03:01:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/program9/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.program9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//LAYOUT - WIFI _ CAMERA _ MUSI CPLAYER _ Progress dialog
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
